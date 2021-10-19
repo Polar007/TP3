e0baf3ae74d23b569283897422ab5b00178fe754
SamuelDePaz <samueldpz@gmail.com>
2021-10-19 15:00:31 +0200

    first push

--- app/src/main/java/com/example/tp3/MainActivity.java
+++ app/src/main/java/com/example/tp3/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.tp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean[] doors = {false,false,false};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
     protected void createRandomDoors(){
         int trueDoorNumber = (int) (Math.random() * 2);
+    }
+
+    public void onButtonPress(View v){
+        Button b = (Button)v;
+        Integer guess = Integer.parseInt(b.getText().toString());
+        int toremove = 0;
+        if (doors[guess]){
+            List doorsnumber = Arrays.asList(1,2,3);
+            doorsnumber.remove(guess);
+            Random rand = new Random();
+            toremove = (int) doorsnumber.get(rand.nextInt(2));
+        }else{
+            List doorsnumber = Arrays.asList(1,2,3);
+            doorsnumber.remove(guess);
+            for (int i = 0; i < 2; i++){
+                int n = (int) doorsnumber.get(i);
+                if (doors[n]){
+                    toremove = n;
+                }
+            }
+        }
+        for (View view:v.getTouchables()){
+            Button button = (Button)view;
+        }
     }
 }
