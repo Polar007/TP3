e7f7da3e853596c48928824f04720b8ea4811f90
SamuelDePaz <samueldpz@gmail.com>
2021-10-19 14:40:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tp3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void createRandomDoors(){
+        int trueDoorNumber = (int) (Math.random() * 2);
+    }
+}
